8d283ceae59ae8b0529f9da87c2f4c23ff3481ad
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-01 01:21:30 +0600

    added create customer, deposit, withdraw, loan functionality

--- src/features/accounts/AccountOperations.jsx
+++ src/features/accounts/AccountOperations.jsx
@@ -1,19 +1,47 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deposit, withdraw, requestLoan, payLoan } from "./accountSlice";
 
 function AccountOperations() {
+  const dispatch = useDispatch();
+  const {
+    loan: currentLoan,
+    loanPurpose: currentLoanPurpose,
+    balance,
+  } = useSelector((store) => store.account);
+
   const [depositAmount, setDepositAmount] = useState("");
   const [withdrawalAmount, setWithdrawalAmount] = useState("");
   const [loanAmount, setLoanAmount] = useState("");
   const [loanPurpose, setLoanPurpose] = useState("");
   const [currency, setCurrency] = useState("USD");
 
-  function handleDeposit() {}
+  function handleDeposit() {
+    if (!depositAmount) return;
 
-  function handleWithdrawal() {}
+    dispatch(deposit(depositAmount));
+    setDepositAmount("");
+  }
 
-  function handleRequestLoan() {}
+  function handleWithdrawal() {
+    if (!withdrawalAmount) return;
 
-  function handlePayLoan() {}
+    dispatch(withdraw(withdrawalAmount));
+    setWithdrawalAmount("");
+  }
+
+  function handleRequestLoan() {
+    if (!loanAmount || !loanPurpose) return;
+
+    dispatch(requestLoan(loanAmount, loanPurpose));
+    setLoanAmount("");
+    setLoanPurpose("");
+  }
+
+  function handlePayLoan() {
+    if (currentLoan > balance) return;
+    dispatch(payLoan());
+  }
 
   return (
     <div>
@@ -33,6 +61,7 @@ function AccountOperations() {
             <option value="USD">US Dollar</option>
             <option value="EUR">Euro</option>
             <option value="GBP">British Pound</option>
+            <option value="BDT">Bangladeshi Taka</option>
           </select>
 
           <button onClick={handleDeposit}>Deposit {depositAmount}</button>
@@ -66,10 +95,14 @@ function AccountOperations() {
           <button onClick={handleRequestLoan}>Request loan</button>
         </div>
 
-        <div>
-          <span>Pay back $X</span>
-          <button onClick={handlePayLoan}>Pay loan</button>
-        </div>
+        {currentLoan > 0 && (
+          <div>
+            <span>
+              Pay back {currentLoan}BDT ({currentLoanPurpose}){" "}
+            </span>
+            <button onClick={handlePayLoan}>Pay loan</button>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/features/accounts/BalanceDisplay.jsx
+++ src/features/accounts/BalanceDisplay.jsx
@@ -1,12 +1,22 @@
+import { connect } from "react-redux";
+
 function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
     style: "currency",
-    currency: "USD",
+    currency: "BDT",
   }).format(value);
 }
 
-function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+// eslint-disable-next-line react/prop-types, react-refresh/only-export-components
+function BalanceDisplay({ balance }) {
+  return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
-export default BalanceDisplay;
+function mapStateToProps(store) {
+  return {
+    balance: store.account.balance,
+  };
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export default connect(mapStateToProps)(BalanceDisplay);
