25eaa69856092c518cd0c4338c2131d2ec505770
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-01 23:21:19 +0600

    implemented redux devtools in classic redux

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
 import reducerAccount from "./features/accounts/accountSlice";
 import reducerCustomer from "./features/customers/customerSlice";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
   account: reducerAccount,
@@ -9,6 +10,9 @@ const rootReducer = combineReducers({
 });
 
 // said to the redux store for applying thunk middleware
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
